1705b9d9427e19daaf2273ade232907e00b891f5
Hari <haricarbajal3@gmail.com>
2024-10-28 18:57:52 +0100

    Mejoras

---
+++ script/main.js
@@ -0,0 +1,278 @@
+
+const noticias = [
+    {
+        "id": 1,
+        "image": "https://ichef.bbci.co.uk/ace/standard/1536/cpsprodpb/f390/live/dd05ab20-9142-11ef-89ae-5575c76d98e6.jpg.webp",
+        "title": "labour volunteers helping Harris",
+        "description": `It's 10 days to go until election day, and the race couldn't be tighter - welcome back to our live US election coverage.
+
+Last night, Kamala Harris appeared alongside singer Beyoncé at a rally in Houston, Texas.
+
+Laying her cards down (down, down, down), Harris made her pitch to some 30,000 people gathered at the event, according to her campaign.
+
+After Trump spoke at a rally in Traverse City, in the swing state of Michigan, last night, his highly-anticipated interview with podcaster Joe Rogan dropped.
+
+During the three-hour long episode, Trump revealed his "biggest mistake" while in office was hiring "bad people or disloyal people" and named his former chief of staff John Kelly and former national security adviser John Bolton as examples.
+
+Both men have been critical of Trump this election cycle and have advised against supporting a second Trump term.
+
+On the penultimate weekend of the campaign, Harris and Trump are pitching to voters in Michigan and Pennsylvania - we'll have all the latest right here.`
+        ,
+        "date": "2023-10-17T11:00:16Z",
+        "category": "Climate"
+    },
+    {
+        "id": 2,
+        "image": "https://ichef.bbci.co.uk/ace/standard/1024/cpsprodpb/2a2a/live/e36c28f0-950b-11ef-9504-b516e8b5f45f.jpg",
+        "title": "Members of Trump's party in Florida have hit out at comments about Puerto Rico by comedian Tony Hinchcliffe.  ",
+        "description": `Vice-President Kamala Harris has hit out at Donald Trump's campaign rally in New York on Sunday, during which a comedian garnered bipartisan backlash for calling US territory Puerto Rico a "floating island of garbage".
+
+Trump is "focused and actually fixated on his grievances, on himself and on dividing our country", she tells reporters gathered on the tarmac as she boards a flight to campaign in Michigan, in what are her first public comments on the rally.
+
+"Nothing about what he is saying that is actually going to support the aspirations, the dreams and the ambitions of the American people," Harris says of her Republican rival.
+
+Americans are "tired" of division and want a president who is "uplifting the people", she adds.
+
+Harris also talks about her own commitment to "paying attention to the needs" of Puerto Rico, including promising to set up a task force focused on the island.
+
+The Trump campaign has distanced itself from Hinchcliffe's comments, saying they "do not reflect" Trump's views.`
+        ,
+        "date": "2024-09-17T10:32:45Z",
+        "category": "Business"
+    },
+    {
+        "id": 3,
+        "image": "https://ichef.bbci.co.uk/ace/standard/1536/cpsprodpb/c4c7/live/176de2f0-91cf-11ef-b3c2-754b6219680e.png.webp",
+        "title": "It's not for us to have shame: Gisele Pelicot takes stand in french mass rape trial",
+        "description": "She tells a court she wants women who have been raped to know that it's not for us to have shame - it's for them",
+        "date": "2024-10-15T10:30:56Z",
+        "category": "Automobile"
+    },
+    {
+        "id": 4,
+        "image": "https://ichef.bbci.co.uk/news/1536/cpsprodpb/e970/live/8d83cdb0-9512-11ef-8e6d-e3e64e16c628.jpg.webp",
+        "title": "Backlash after comedian at Trump rally calls Puerto Rico 'island of garbage",
+        "description": `A comedian at a Donald Trump rally called Puerto Rico a "floating island of garbage", sparking fury from Republicans and Democrats and accusations of racism.
+
+The comic, Tony Hinchcliffe, was among the speakers at the Madison Square Garden in New York on Sunday. He also made a series of jokes that leant on racist stereotypes.
+
+A Trump adviser distanced the former president from the Puerto Rico joke, which was also denounced by Trump's Democratic rival, Kamala Harris. Harris herself was the target of another Trump warm-up speaker who also sparked controversy.
+
+The furore came as one of the world's top Latin celebrities, Puerto Rican rapper Bad Bunny, endorsed Harris for president.`,
+        "date": "2024-10-13T10:25:24Z",
+        "category": "Automobile"
+    },
+    {
+        "id": 5,
+        "image": "https://ichef.bbci.co.uk/news/1024/cpsprodpb/3e81/live/e174ad80-91d0-11ef-8581-cd31a4127fee.jpg.webp",
+        "title": "India evacuating more than a million people as Cyclone Dana nears",
+        "description": "Authorities in India are evacuating nearly 1.5 million people from the path of an approaching cyclone in the eastern states of Odisha (formerly Orissa) and West Bengal.",
+        "date": "2024-10-01T10:22:56Z",
+        "category": "Technology"
+    },
+    {
+        "id": 6,
+        "image": "https://ichef.bbci.co.uk/news/1536/cpsprodpb/c509/live/f5610bc0-931c-11ef-b22c-69d6fbdd7891.jpg.webp",
+        "title": "Mining guiants sign $30bn",
+        "description": "The dam collapse released toxic waste and mud, which flooded nearby towns, rivers and forests",
+        "date": "2024-10-01T10:22:56Z",
+        "category": "Technology"
+    },
+    {
+        "id": 7,
+        "image": "https://ichef.bbci.co.uk/images/ic/1024xn/p0jzgv06.jpg.webp",
+        "title": "An Irish banjo virtuoso's guide to the best traditional Irish music experiences in Galway",
+        "description": "Galway, with its narrow medieval streets, vibrant pub culture and unique brand of craic (lively fun), also boasts one of Ireland's best music scenes.",
+        "date": "2024-10-01T10:22:56Z",
+        "category": "Technology"
+    },
+    {
+        "id": 8,
+        "image": "https://ichef.bbci.co.uk/news/1536/cpsprodpb/1fd3/live/5cebfca0-908e-11ef-a624-634f82a66078.jpg.webp",
+        "title": "Tariffs hurt his business. He's voting for Trump anyway",
+        "description": `For almost 35 years, Wyoming entrepreneur Alan Chadwick has run his business importing clothing from China and selling the Western-style gear to stores serving "working cowboys" in the US.
+        Now, as former President Donald Trump campaigns on a pledge to hit all goods coming into the country with a 10%-20% tariff, or border tax, which would rise to 60% for goods from China, Chadwick is having to drastically rethink his strategy.
+        The 66-year-old has been exploring moving manufacturing of his products, like wool shirts with snaps and canvas jackets, to India or Pakistan - or perhaps closing his Wyoming Traders business, which employs 16 people, and retiring altogether.
+        Chadwick said tariffs were a "tax on the American people" and warned that the expense for a company like his of opening a factory in the US was unrealistic.
+        But as he prepares to cast his ballot, he expects to swallow his qualms about tariffs in favour of other priorities, such as illegal immigration and opposition to abortion.
+        "I will vote for Trump even though he's going to hurt our company if he does what he says he's going to do," he said.`,
+        "date": "2024-10-01T10:22:56Z",
+        "category": "Technology"
+    }
+]
+
+class CustomSearch extends HTMLElement {
+    constructor() {
+        super();
+        this.noticias = noticias;
+    }
+
+    connectedCallback() {
+        const dialogBtn = document.querySelector('.dialog-search');
+        const closeBtn = this.querySelector('.close-btn');
+        const dialog = this.querySelector('dialog');
+
+        dialogBtn.addEventListener('click', () => {
+            dialog.show(); // Mostrar el diálogo
+            setTimeout(() => dialog.classList.add('showing'), 10); // Activar la animación
+        });
+
+        closeBtn.addEventListener('click', () => {
+            dialog.classList.remove('showing'); // Desactivar la animación
+            setTimeout(() => dialog.close(), 500); // Esperar la animación y luego cerrar
+        });
+
+        const siteSearch = this.querySelector('#site-search');
+        siteSearch.addEventListener('input', (event) => this.search(event));
+
+        this.renderResults(''); // Muestra los resultados al cargar
+    }
+
+    search(event) {
+        event.preventDefault();
+        const siteSearch = this.querySelector('#site-search');
+        const term = siteSearch.value;
+        this.renderResults(term);
+    }
+
+    renderResults(term = '') {
+        const ul = this.querySelector("#search-results");
+        ul.innerHTML = '';
+        const noticia = this.noticias
+            .filter(articulo => articulo.title.toLowerCase().includes(term.toLowerCase()))
+
+        const template = this.querySelector('template').content;
+
+        noticia.map(insertar => {
+            const li = template.querySelector("li").cloneNode(true)
+            li.querySelector(".card .item-image").src = insertar.image;
+            li.querySelector(".card .item-title a").textContent = insertar.title;
+            li.querySelector(".card .item-description").textContent = insertar.description;
+            li.querySelector("relative-time").setAttribute('time', insertar.date);
+
+            const a = li.querySelector(".card .item-title a");
+            a.href = a.href.replace('{id}', insertar.id);
+
+            ul.appendChild(li);
+        })
+
+    }
+}
+
+customElements.define('custom-search', CustomSearch);
+
+
+class RelativeTime extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.render();
+        setInterval(() => this.render(), 1000);
+    }
+    render() {
+        const timeValue = this.getAttribute('time')
+        const time = timeValue ? new Date(Number(timeValue)).getTime() : Date.now();
+        const now = Date.now();
+
+        const diff = now - time;
+        const seconds = Math.floor(diff / 1000) || 1;
+        const minutes = Math.floor(diff / (1000 * 60));
+        const hours = Math.floor(diff / (1000 * 60 * 60));
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(months / 12);
+
+        let aux = '...';
+        if (months >= 12) {
+            aux = `Hace ${years} año${years > 1 ? 's' : ''}`
+        } else if (days > 30 && months >= 1) {
+            aux = `Hace ${months} mes${months > 1 ? 'es' : ''}`
+        } else if (days >= 1) {
+            aux = `Hace ${days} día${days > 1 ? 's' : ''}`
+        } else if (hours >= 1) {
+            aux = `Hace ${hours} hora${hours > 1 ? 's' : ''}`
+        } else if (minutes >= 1) {
+            aux = `Hace ${minutes} minuto${minutes > 1 ? 's' : ''}`
+        } else if (seconds >= 1) {
+            aux = `Hace ${seconds} segundo${seconds > 1 ? 's' : ''}`
+        }
+
+        this.textContent = aux;
+    }
+}
+
+customElements.define('relative-time', RelativeTime);
+const botonNavegacion = document.querySelectorAll(".boton-navegacion");
+botonNavegacion.forEach(button => {
+    button.addEventListener("mouseover", function () {
+        button.classList.add("nuevo-boton-navegacion");
+    });
+
+    button.addEventListener("mouseout", function () {
+        button.classList.remove("nuevo-boton-navegacion");
+    });
+});
+
+class MenuDialog extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const MenuDialog = this.querySelector(".contenedor-dialog-menu");
+        const botonAbrir = document.querySelector(".imagen-menu");
+        const botonesMenu = this.querySelector("boton-menu-dialog");
+
+        botonAbrir.addEventListener("click", function () {
+            MenuDialog.classList.toggle("mostrar-contenedor");
+            if (botonAbrir.src.endsWith("cerrar.png")) {
+                botonAbrir.src = "styles/lista.png"; // Cambiamos a lista.png
+                botonAbrir.classList.remove("rotar"); // Quitar la rotación
+            } else {
+                botonAbrir.src = "styles/cerrar.png"; // Cambiamos a cerrar.png
+                botonAbrir.classList.add("rotar"); // Agregar la rotación
+            }
+        });
+
+        botonesMenu.addEventListener("mouseover", function () {
+            botonesMenu.style.backgroundColor = 'rgba(230, 230, 230, 0.53)';
+        });
+
+
+    }
+}
+customElements.define("custom-menu", MenuDialog)
+
+
+// Definir getId para obtener el id desde la URL
+function getId() {
+    return new URLSearchParams(window.location.search).get("id");
+}
+
+class CustomArticle extends HTMLElement {
+    constructor() {
+        super();
+        this.articleId = Number(getId()); // Asegurarse que sea un número
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        const article = noticias.find(article => article.id === this.articleId);
+        if (article) {
+            const h1 = this.querySelector('h1');
+            h1.textContent = article.title;
+            const imagen = this.querySelector("img");
+            imagen.src = article.image;
+            const description = this.querySelector("p");
+            description.textContent = article.description;
+            const time = this.querySelector("relative-time");
+            time.setAttribute('time', article.date);
+
+        }
+    }
+}
+customElements.define('custom-article', CustomArticle);
